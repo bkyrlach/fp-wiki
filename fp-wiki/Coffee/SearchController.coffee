066d3aed2758a3eaa37c5f8f316054a2cab9e6ba
phdlove42 <phdlove42@gmail.com>
2016-02-26 15:43:20 -0500

    I added the context as well as modifications to the front end

--- fp-wiki/Coffee/SearchController.coffee
+++ fp-wiki/Coffee/SearchController.coffee
@@ -3,6 +3,17 @@
 Site.controller 'SearchController', ['$scope', 'SearchApi', ($scope, searchApi) ->
   $scope.greeting = 'Hello, world'
   $scope.query = ''
-  $scope.search = () -> searchApi.get { query: $scope.query } , (result) -> console.log(result)
+  $scope.methods = null
+  $scope.search = () -> 
+    $scope.methods = null
+    searchApi.get { query: $scope.query } , 
+      (result) -> 
+        $scope.methods = result
+        console.log($scope.methods)
+
+  $scope.goToDetails = (method) ->
+    searchApi.get {id: method.Id} ,
+      (result) ->
+        $scope.details = result
   true
 ]
--- fp-wiki/Controllers/SearchController.cs
+++ fp-wiki/Controllers/SearchController.cs
@@ -3,22 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
+using fp_wiki.DataContext;
 
 namespace fp_wiki.Controllers
 {
     public class SearchController : ApiController
     {
-        public List<string> Get(Search search)
+        private FpWikiDataContext _dataContext = new FpWikiDataContext();
+
+        public IHttpActionResult Get(Search search)
         {
-            Console.WriteLine(search);
-            return new List<string>
-            {
-                "Foo",
-                "Bar",
-                "Baz",
-                "FooBar"
-            };
-        } 
+            return Ok(_dataContext.Set<MethodDescriptor>().ToList());
+        }
+
+        public IHttpActionResult GetById(int id)
+        {
+            return Ok(_dataContext.Set<MethodDescriptor>().Where(md => md.Id == id));
+        }
     }
 
     public class Search
